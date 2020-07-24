63798683f55b06ef76ff1b92be3d0c10423cdd2f
Justin Carter (video) <justicart@fb.com>
2020-07-24 13:22:51 -0400

    Anthony

---
+++ src/routes/posts/2020-07-24-Anthony/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Anthony`,
+  tags: ['gpt-3', 'time machine'],
+  spoiler: "Anthony comes up with a bunch of witty things to say.",
+  getContent: () => import('./document.mdx'),
+}
