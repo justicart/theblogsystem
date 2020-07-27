64a1cae03717048453e451b2c6ac27c8fb13cbac
Justin Carter <justin@vidpresso.com>
2020-07-26 22:30:01 -0400

    star-crossed-trekkers

---
+++ src/routes/posts/2020-07-26-star-crossed-trekkers/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Star-crossed Trekkers`,
+  tags: ['gpt-3', 'star trek'],
+  spoiler: "Read and find out who Spock ends up with in the end and why.",
+  getContent: () => import('./document.mdx'),
+}
