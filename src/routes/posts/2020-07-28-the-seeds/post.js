1a5d28dd4a598d39f4d9d45310ba564e7e41b3f7
Justin Carter <justin@vidpresso.com>
2020-07-28 23:05:54 -0400

    the gift of the seed

---
+++ src/routes/posts/2020-07-28-the-seeds/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `The Gift of the Seed`,
+  tags: ['gpt-3', 'seeds', 'conspiracy'],
+  spoiler: "It is my hope that one day, all people will live in freedom",
+  getContent: () => import('./document.mdx'),
+}
