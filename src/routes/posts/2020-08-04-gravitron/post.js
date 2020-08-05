48df81af6a91f28310370912a2d0de1191c640ec
Justin Carter <justin@vidpresso.com>
2020-08-04 23:21:46 -0400

    gravitron

---
+++ src/routes/posts/2020-08-04-gravitron/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Gravitron`,
+  tags: ['gpt-3', 'fair', 'keller'],
+  spoiler: "You won't belive this weird trick to cure vertigo",
+  getContent: () => import('./document.mdx'),
+}
