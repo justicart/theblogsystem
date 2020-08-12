06e738888886c3cda19f05b4c5666b95bb362891
Justin Carter <justin@vidpresso.com>
2020-08-12 17:43:13 -0400

    bill

---
+++ src/routes/posts/2020-08-12-the-juggling-ball/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Bill, the Juggling Ball`,
+  tags: ['juggling', 'personification'],
+  spoiler: "I told him that I was too dizzy.",
+  getContent: () => import('./document.mdx'),
+}
