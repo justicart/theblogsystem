6e05ca9914f6ce99fc0f6f5d4f23a8fd9b46ca17
Justin Carter <justin@vidpresso.com>
2020-08-21 00:23:19 -0400

    Revert "hurricane race"
    This reverts commit 60560cce0e34741b20d486ca9f352515e0c2921a.

--- src/routes/posts/2020-08-20-hurricane-race/post.js
+++
@@ -1,6 +0,0 @@
-export default {
-title: `Hurricane Race`,
-tags: ['weather', 'personification', 'hurricane'],
-spoiler: "We don't know if she'll move off to the north. We don't know if she'll move off to the south. We don't know if she'll move toward the west. We don't know if she'll move toward the east. We just don't know.",
-getContent: () => import('./document.mdx'),
-}
