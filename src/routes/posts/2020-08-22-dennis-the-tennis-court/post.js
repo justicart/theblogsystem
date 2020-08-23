b6c2ab256ac4239c7ff461adc4b8e6ce71ab3305
Justin Carter <github@justicart.com>
2020-08-22 22:41:38 -0400

    dennis

---
+++ src/routes/posts/2020-08-22-dennis-the-tennis-court/post.js
@@ -0,0 +1,6 @@
+export default {
+title: `Dennis the Tennis Court`,
+tags: ['tennis', 'personification'],
+spoiler: '"Don\'t take me!" cried Dennis. "I\'m not all here anymore. I\'m falling apart. Save the children, save the children!"',
+getContent: () => import('./document.mdx'),
+}
