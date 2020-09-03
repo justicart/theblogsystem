cec3a02e3e91835b54c07a01a7f2f160a301111d
Justin Carter <justin@vidpresso.com>
2020-09-03 17:12:23 -0400

    bigfoot

---
+++ src/routes/posts/2020-09-03-bigfoot/post.js
@@ -0,0 +1,6 @@
+export default {
+title: `Bigfoot`,
+tags: ['bigfoot', 'personification'],
+spoiler: "I'm not going to hit you or punch you. I'm ready to make friends.",
+getContent: () => import('./document.mdx'),
+}
