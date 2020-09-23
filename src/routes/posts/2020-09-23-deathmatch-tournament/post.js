70bef1e078eda890a6306babc96462d1363dfb95
Justin Carter <github@justicart.com>
2020-09-23 09:29:14 -0400

    deathmatch tournament

---
+++ src/routes/posts/2020-09-23-deathmatch-tournament/post.js
@@ -0,0 +1,6 @@
+export default {
+title: `Deathmatch Tournament`,
+tags: ['rats', 'personification', 'review'],
+spoiler: "It's a story about how money creates heroes.",
+getContent: () => import('./document.mdx'),
+}
