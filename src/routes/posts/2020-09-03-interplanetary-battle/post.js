17d9891a4c47e5f4e901a39350d1d7b7cae77c09
Justin Carter <justin@vidpresso.com>
2020-10-05 17:50:10 -0400

    interplanetary battle

---
+++ src/routes/posts/2020-09-03-interplanetary-battle/post.js
@@ -0,0 +1,6 @@
+export default {
+title: `Interplanetary Battle`,
+tags: ['planets', 'solar system', 'personification'],
+spoiler: "We’re waiting for the Moon to attack.",
+getContent: () => import('./document.mdx'),
+}
