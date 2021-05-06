b54f12119aa063ecd6f0d3d0406fb46ca7e53f44
Justin Carter <justin@vidpresso.com>
2021-05-05 23:51:01 -0400

    fish cat

---
+++ src/routes/posts/2021-05-05-no-tacos-go-to-heaven/post.js
@@ -0,0 +1,6 @@
+export default {
+title: `No Tacos Go to Heaven`,
+tags: ['tacos', 'food', 'personification'],
+spoiler: "Wouldn't we be happier if we could just stop fighting over tacos?",
+getContent: () => import('./document.mdx'),
+}
