a8d960c77e1f571287b2b1ef764ec14783d5a5a0
Justin Carter <github@justicart.com>
2022-12-08 20:04:12 -0500

    the hot dog and the dragon

---
+++ src/routes/posts/2022-12-08-the-hot-dog-and-the-dragon/post.js
@@ -0,0 +1,6 @@
+export default {
+title: `The Hot Dog and the Dragon`,
+tags: ['hot dog', 'food', 'dragon'],
+spoiler: "He realized that he had been foolish to think that dictatorship was a good idea.",
+getContent: () => import('./document.mdx'),
+}
