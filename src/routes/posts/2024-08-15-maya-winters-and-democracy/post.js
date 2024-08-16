d311c5693a3cf74c633b493efcb86a7a1e150ec5
Justin Carter <github@justicart.com>
2024-08-15 23:41:58 -0400

    add dystopian story

---
+++ src/routes/posts/2024-08-15-maya-winters-and-democracy/post.js
@@ -0,0 +1,17 @@
+export default {
+  title: "Exposing Skimm the Vote: Maya Winters' Fight for Democracy",
+  tags: [
+    "VotingRights",
+    "Democracy2024",
+    "PoliticalThriller",
+    "FemaleEmpowerment",
+    "GovernmentConspiracy",
+    "ElectionIntegrity",
+    "Activism",
+    "MayaWinters",
+    "SkimmTheVote",
+  ],
+  spoiler:
+    "In a dystopian 2024, journalist Maya Winters uncovers a sinister plot to suppress female voters and fights to protect democracy.",
+  getContent: () => import("./document.mdx"),
+};
